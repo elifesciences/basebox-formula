e8ea08604d9d703142e5dc31e237aa0f88826c2a
Giorgio Sironi <g.sironi@elifesciences.org>
2019-05-16 11:53:44 +0100

    Implement a reliable ~5 second worker cycle

--- salt/basebox/config/test.py
+++ salt/basebox/config/test.py
@@ -2,6 +2,7 @@ import syslog
 import os
 import sys
 import signal
+import time
 
 pid = os.getpid()
 aid = sys.argv[1:2]
@@ -16,7 +17,7 @@ class Flag:
 flag = Flag()
 
 def signal_handler(signum, _frame):
-    print("received signal %s", signum)
+    syslog.syslog("received signal %s" % signum)
     flag.stop()
 
 signal.signal(signal.SIGTERM, signal_handler)
@@ -27,11 +28,10 @@ while not flag.should_stop:
     iteration += 1
     message = "aid: %s, pid: %s, iteration: %s" % (aid, pid, iteration)
     syslog.syslog(message)
-    print("start of long operation")
-    # long operation that is not interrupted by signals like time.sleep()
-    for i in range(0, 1000000):
-        with open('/dev/null') as fp:
-            fp.read()
-    print("end of long operation")
+    syslog.syslog("start of long operation")
+    # long operation that persists even if interrupted by signals in time.sleep()
+    for i in range(0, 5):
+        time.sleep(1)
+    syslog.syslog("end of long operation")
 
-print("gracefully stopping")
+syslog.syslog("gracefully stopping")
