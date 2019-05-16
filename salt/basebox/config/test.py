34a54d01b11d6efe5222e8db8906850a0fa57d4b
Giorgio Sironi <g.sironi@elifesciences.org>
2019-05-16 11:19:03 +0100

    Properly wait on SIGTERM and exit gracefully

--- salt/basebox/config/test.py
+++ salt/basebox/config/test.py
@@ -1,14 +1,37 @@
 import syslog
 import os
 import sys
-import time
+import signal
 
 pid = os.getpid()
 aid = sys.argv[1:2]
 
+class Flag:
+    def __init__(self):
+        self.should_stop = False
+
+    def stop(self):
+        self.should_stop = True
+
+flag = Flag()
+
+def signal_handler(signum, _frame):
+    print("received signal %s", signum)
+    flag.stop()
+
+signal.signal(signal.SIGTERM, signal_handler)
+
+
 iteration=0
-while True:
+while not flag.should_stop:
     iteration += 1
     message = "aid: %s, pid: %s, iteration: %s" % (aid, pid, iteration)
     syslog.syslog(message)
-    time.sleep(5)
+    print("start of long operation")
+    # long operation that is not interrupted by signals like time.sleep()
+    for i in range(0, 1000000):
+        with open('/dev/null') as fp:
+            fp.read()
+    print("end of long operation")
+
+print("gracefully stopping")
